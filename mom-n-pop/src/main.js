e4a74a380efcfc8a935d0f7f9472ef5846a97d57
Karl Pokus <karl.pokus@gmail.com>
2017-02-05 15:53:48 +0100

    add render function option

--- mom-n-pop/src/main.js
+++ mom-n-pop/src/main.js
@@ -8,6 +8,10 @@ new Vue({
   el: '#app',
   router: router,
   store: store,
+  /* https://github.com/vuejs/vueify#configuring-options
+  render: function (createElement) {
+    return createElement(App)
+  }*/
   template: '<app/>', // is this the template part of the app export?
   components: {
     app
